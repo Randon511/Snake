8a4bb35f20538071de7b29dd9b5227f6140f5686
Randon <randon551@gmail.com>
2019-12-29 15:52:18 -0800

    Grid

--- Gamepanel.java
+++ Gamepanel.java
@@ -1,5 +1,6 @@
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 public class Gamepanel extends JPanel
 {
@@ -10,4 +11,38 @@ public class Gamepanel extends JPanel
 	{
 		setPreferredSize(new Dimension(WIDTH,HEIGHT));
 	}
+
+	public void paint(Graphics g)
+	{
+		//Horizontal Lines
+		for(int i = 0; i < WIDTH/25; i++)
+		{
+			g.drawLine(i*25,0,i*25,HEIGHT);
+		}
+
+		//Vertical Lines
+		for(int i = 0; i < HEIGHT/25; i++)
+		{
+			g.drawLine(0,i*25,WIDTH,i*25);
+		}
+	}
+
+
+	public void start()
+	{
+	}
+
+	public void stop()
+	{
+	}
+
+	public void tick()
+	{
+	}
+
+	public void run()
+	{
+	}
 }
+
+
